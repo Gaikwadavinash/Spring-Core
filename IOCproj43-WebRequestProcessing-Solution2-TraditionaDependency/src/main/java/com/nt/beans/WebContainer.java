235b1f279424f0cb6a4bd97a9ea48a1cf8a603f7
My <My@AVINASH>
2021-04-03 21:40:58 +0530

    WebRequestProcessing-Solution2-TraditionaDependency

--- IOCproj43-WebRequestProcessing-Solution2-TraditionaDependency/src/main/java/com/nt/beans/WebContainer.java
+++ IOCproj43-WebRequestProcessing-Solution2-TraditionaDependency/src/main/java/com/nt/beans/WebContainer.java
@@ -1,10 +1,13 @@
 package com.nt.beans;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class WebContainer {
+public class WebContainer implements ApplicationContextAware {
 private String beanId;
+private ApplicationContext ctx;
 
 public WebContainer(String beanId) {
 	System.out.println("WebContainer 1-param Constructor");
@@ -14,12 +17,19 @@ public WebContainer(String beanId) {
 public void requestHandle(String data) {
 System.out.println("WebContainer Data::"+data);
 System.out.println("WebContainer.hashcode::"+this.hashCode());
-//create ioc Container
-ApplicationContext ctx= 
-new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+////create ioc Container
+//ApplicationContext ctx= 
+//new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
 System.out.println("2nd IOC Conatiner");
 RequestHandler rh= ctx.getBean("handler",RequestHandler.class);
 rh.processingData(data);
 System.out.println("WebContainer Data::"+data);
+}
+
+@Override
+public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+	System.out.println("WebContainer.setApplicationContext()");
+	this.ctx=ctx;
+	
 }
 }
